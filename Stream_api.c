1c96608b9629cc63c18780019f13a4ac0a04bc26
Tamas Farkas <farkas.tamas@gmail.com>
2020-04-22 03:48:11 +0200

    file to file encoder

--- Stream_api.c
+++ Stream_api.c
@@ -1,185 +1,193 @@
-// example appsrc for gstreamer 1.0 with own mainloop & external buffers. based on example from gstreamer docs.
-// public domain, 2015 by Florian Echtler <floe@butterbrot.org>. compile with:
-// gcc --std=c99 -Wall $(pkg-config --cflags gstreamer-1.0) -o gst gst.c $(pkg-config --libs gstreamer-1.0) -lgstapp-1.0
-
 #include <gst/gst.h>
-#include <gst/app/gstappsrc.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include <stdint.h>
-
-
-#define imgwidth 1920
-#define imgheight 1080
+#define CHUNK_SIZE 1080  /* Amount of bytes we are sending in each buffer */
 
 /* Structure to contain all our information, so we can pass it to callbacks */
-typedef struct _CustomData
-{
-  GstElement *pipeline;
-  GstElement *source;
-  GstElement *converter;
-  GstElement *encoder;
-  GstElement *packager;
-  GstElement *sink;
+typedef struct _CustomData {
+  GstElement *pipeline, *app_source;
+  GstElement *rawvideoparse, *video_convert, *encoder, *video_sink;
+
+  GMappedFile *file;
+  guint8 *data;
+  gsize length;
+  guint64 offset;
+
+  guint sourceid;        /* To control the GSource */
+
+  GMainLoop *main_loop;  /* GLib's Main Loop */
 } CustomData;
 
-
-int want = 1;
-
-uint16_t b_white[imgwidth*imgheight];
-uint16_t b_black[imgwidth*imgheight];
-
-static void prepare_buffer(GstAppSrc* appsrc) {
-
-  static gboolean white = FALSE;
-  static GstClockTime timestamp = 0;
+/* This method is called by the idle GSource in the mainloop, to feed CHUNK_SIZE bytes into appsrc.
+ * The idle handler is added to the mainloop when appsrc requests us to start sending data (need-data signal)
+ * and is removed when appsrc has enough data (enough-data signal).
+ */
+static gboolean push_data (CustomData *data) {
   GstBuffer *buffer;
-  guint size;
   GstFlowReturn ret;
+  guint len;
 
-  if (!want) return;
-  want = 0;
+  if (data->offset >= data->length) {
+    /* we are EOS, send end-of-stream and remove the source */
+    g_signal_emit_by_name (data->app_source, "end-of-stream", &ret);
+    g_main_loop_quit(data->main_loop);   //FIXME
+    return FALSE;
+  }
 
-  size = imgwidth * imgheight * 2;
+  /* read the next chunk */
+  buffer = gst_buffer_new ();
 
-  buffer = gst_buffer_new_wrapped_full( 0, (gpointer)(white?b_white:b_black), size, 0, size, NULL, NULL );
+  len = CHUNK_SIZE;
+  if (data->offset + len > data->length)
+    len = data->length - data->offset;
 
-  white = !white;
+    gst_buffer_append_memory (buffer,
+        gst_memory_new_wrapped (GST_MEMORY_FLAG_READONLY,
+            data->data, data->length, data->offset, len, NULL, NULL));
 
-  GST_BUFFER_PTS (buffer) = timestamp;
-  GST_BUFFER_DURATION (buffer) = gst_util_uint64_scale_int (1, GST_SECOND, 4);
 
-  timestamp += GST_BUFFER_DURATION (buffer);
+  /* Push the buffer into the appsrc */
+  g_signal_emit_by_name (data->app_source, "push-buffer", buffer, &ret);
 
-  ret = gst_app_src_push_buffer(appsrc, buffer);
+  /* Free the buffer now that we are done with it */
+  gst_buffer_unref (buffer);
 
   if (ret != GST_FLOW_OK) {
-    /* something wrong, stop pushing */
-    // g_main_loop_quit (loop);
+    /* We got some error, stop sending data */
+    return FALSE;
+  }
+
+  data->offset += len;
+
+  return TRUE;
+}
+
+/* This signal callback triggers when appsrc needs data. Here, we add an idle handler
+ * to the mainloop to start pushing data into the appsrc */
+static void start_feed (GstElement *source, guint size, CustomData *data) {
+  if (data->sourceid == 0) {
+    g_print ("Start feeding\n");
+    data->sourceid = g_idle_add ((GSourceFunc) push_data, data);
   }
 }
 
-static void cb_need_data (GstElement *appsrc, guint unused_size, gpointer user_data) {
-  //prepare_buffer((GstAppSrc*)appsrc);
-  want = 1;
+/* This callback triggers when appsrc has enough data and we can stop sending.
+ * We remove the idle handler from the mainloop */
+static void stop_feed (GstElement *source, CustomData *data) {
+  if (data->sourceid != 0) {
+    g_print ("Stop feeding\n");
+    g_source_remove (data->sourceid);
+    data->sourceid = 0;
+  }
 }
 
-gint main (gint argc, gchar *argv[]) {
 
+/* This function is called when an error message is posted on the bus */
+static void error_cb (GstBus *bus, GstMessage *msg, CustomData *data) {
+  GError *err;
+  gchar *debug_info;
+
+  /* Print error details on the screen */
+  gst_message_parse_error (msg, &err, &debug_info);
+  g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+  g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+  g_clear_error (&err);
+  g_free (debug_info);
+
+  g_main_loop_quit (data->main_loop);
+}
+
+int main(int argc, char *argv[]) {
   CustomData data;
   GstBus *bus;
-  GstMessage *msg;
-  GstStateChangeReturn ret;
-  gboolean terminate = FALSE;
+  GError *error = NULL;
 
-  for (int i = 0; i < imgwidth * imgheight; i++) { b_black[i] = 0; b_white[i] = 0xFFFF; }
+  /* Initialize cumstom data structure */
+  memset (&data, 0, sizeof (data));
 
-  /* init GStreamer */
+  /* Initialize GStreamer */
   gst_init (&argc, &argv);
 
+  if (argc < 2) {
+    g_print ("usage: %s <filename>\n", argv[0]);
+    return -1;
+  }
+
+  CustomData *datap = &data;
+  /* try to open the file as an mmapped file */
+  datap->file = g_mapped_file_new (argv[1], FALSE, &error);
+  if (error) {
+    g_print ("failed to open file: %s\n", error->message);
+    g_error_free (error);
+    return -2;
+  }
+  /* get some vitals, this will be used to read data from the mmapped file and
+   * feed it to appsrc. */
+  data.length = g_mapped_file_get_length (data.file);
+  data.data = (guint8 *) g_mapped_file_get_contents (data.file);
+  data.offset = 0;
+
   /* Create the elements */
-  data.source = gst_element_factory_make ("appsrc", "source");
-  data.converter = gst_element_factory_make ("videoconvert", "converter");
+  data.app_source = gst_element_factory_make ("appsrc", "video_source");
+  data.rawvideoparse = gst_element_factory_make ("rawvideoparse", "rawvideoparse");
+  data.video_convert = gst_element_factory_make ("videoconvert", "video_convert");
   data.encoder = gst_element_factory_make ("x265enc", "encoder");
-  data.packager = gst_element_factory_make ("rtph265pay", "packager");
-  data.sink = gst_element_factory_make ("udpsink", "sink");
-//data.sink = gst_element_factory_make ("xvimagesink", "sink");
+  data.video_sink = gst_element_factory_make ("filesink", "video_sink");
 
   /* Create the empty pipeline */
   data.pipeline = gst_pipeline_new ("test-pipeline");
 
-  if (!data.pipeline || !data.source || !data.converter || !data.encoder || !data.packager || !data.sink) {
+  if (!data.pipeline || !data.app_source || !data.rawvideoparse || !data.video_convert || !data.encoder || !data.video_sink) {
     g_printerr ("Not all elements could be created.\n");
     return -1;
   }
 
-  /* setup */
-  g_object_set (G_OBJECT (data.source), "caps",
-  		gst_caps_new_simple ("video/x-raw",
-				     "format", G_TYPE_STRING, "I420",
-				     "width", G_TYPE_INT, imgwidth,
-				     "height", G_TYPE_INT, imgheight,
-				     "framerate", GST_TYPE_FRACTION, 60, 1,
-				     NULL), NULL);
-  /* Build the pipeline. Note that we are NOT linking the source at this
-  * point. We will do it later. */
-  gst_bin_add_many (GST_BIN (data.pipeline), data.source, data.converter, data.encoder, data.packager, data.sink, NULL);
-  if (!gst_element_link_many (data.source, data.converter, data.encoder, data.packager, data.sink, NULL)) {
+  g_object_set (data.video_sink, "location", "/home/tamas/Documents/Dipterv/out.h265" , NULL);
+
+  /* Configure appsrc */
+  /*g_object_set (G_OBJECT (data.app_source), "caps",
+   		gst_caps_new_simple ("video/x-raw",
+ 				     "format", G_TYPE_STRING, "I420",
+ 				     "width", G_TYPE_INT, 1920,
+ 				     "height", G_TYPE_INT, 1080,
+ 				     "framerate", GST_TYPE_FRACTION, 60, 1,
+ 				     NULL), NULL);
+*/
+  g_signal_connect (data.app_source, "need-data", G_CALLBACK (start_feed), &data);
+  g_signal_connect (data.app_source, "enough-data", G_CALLBACK (stop_feed), &data);
+
+  //g_object_set (data.rawvideoparse, "format", "I420", NULL);
+  g_object_set (data.rawvideoparse, "width", 1920, NULL);
+  g_object_set (data.rawvideoparse, "height", 1080, NULL);
+  g_object_set (data.rawvideoparse, "framerate", 60, 1, NULL);
+
+  /* Link all elements that can be automatically linked because they have "Always" pads */
+  gst_bin_add_many (GST_BIN (data.pipeline), data.app_source, data.rawvideoparse, data.video_convert, data.encoder, data.video_sink, NULL);
+  if (gst_element_link_many (data.app_source, data.rawvideoparse, data.video_convert, data.encoder, data.video_sink, NULL) != TRUE ) {
     g_printerr ("Elements could not be linked.\n");
     gst_object_unref (data.pipeline);
     return -1;
   }
 
-  /* setup appsrc */
-  g_object_set (G_OBJECT (data.source),
-		"stream-type", 0, // GST_APP_STREAM_TYPE_STREAM
-		"format", GST_FORMAT_TIME,
-    "is-live", TRUE,
-    NULL);
-  g_signal_connect (data.source, "need-data", G_CALLBACK (cb_need_data), NULL);
-
-  g_object_set (data.packager, "config-interval", 3, NULL);
-  g_object_set (data.sink, "clients", "127.0.0.1:5200", NULL);
-  g_object_set (data.sink, "sync", FALSE, NULL);
-
-  /* Start playing */
-  ret = gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
-  if (ret == GST_STATE_CHANGE_FAILURE) {
-    g_printerr ("Unable to set the pipeline to the playing state.\n");
-    gst_object_unref (data.pipeline);
-    return -1;
-  }
-
-/* Listen to the bus */
+  /* Instruct the bus to emit signals for each received message, and connect to the interesting signals */
   bus = gst_element_get_bus (data.pipeline);
-do {
-  msg = gst_bus_pop_filtered (bus,
-      GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-  /* Parse message */
-  if (msg != NULL) {
-    GError *err;
-    gchar *debug_info;
+  gst_bus_add_signal_watch (bus);
+  g_signal_connect (G_OBJECT (bus), "message::error", (GCallback)error_cb, &data);
+  gst_object_unref (bus);
 
-    switch (GST_MESSAGE_TYPE (msg)) {
-      case GST_MESSAGE_ERROR:
-        gst_message_parse_error (msg, &err, &debug_info);
-        g_printerr ("Error received from element %s: %s\n",
-            GST_OBJECT_NAME (msg->src), err->message);
-        g_printerr ("Debugging information: %s\n",
-            debug_info ? debug_info : "none");
-        g_clear_error (&err);
-        g_free (debug_info);
-        terminate = TRUE;
-        break;
-      case GST_MESSAGE_EOS:
-        g_print ("End-Of-Stream reached.\n");
-        terminate = TRUE;
-        break;
-      case GST_MESSAGE_STATE_CHANGED:
-        /* We are only interested in state-changed messages from the pipeline */
-        if (GST_MESSAGE_SRC (msg) == GST_OBJECT (data.pipeline)) {
-          GstState old_state, new_state, pending_state;
-          gst_message_parse_state_changed (msg, &old_state, &new_state,
-              &pending_state);
-          g_print ("Pipeline state changed from %s to %s:\n",
-              gst_element_state_get_name (old_state),
-              gst_element_state_get_name (new_state));
-        }
-        break;
-      default:
-        /* We should not reach here */
-        g_printerr ("Unexpected message received.\n");
-        break;
-    }
-    gst_message_unref (msg);
-  }
+  fprintf(stderr, "Setting main_loop_run to GST_STATE_PLAYING\n");
+  /* Start playing the pipeline */
+  gst_element_set_state (data.pipeline, GST_STATE_PLAYING);
 
-  prepare_buffer((GstAppSrc*)data.source);
-  g_main_context_iteration(g_main_context_default(),FALSE);
+  /* Create a GLib Main Loop and set it to run */
+  data.main_loop = g_main_loop_new (NULL, FALSE);
+  g_main_loop_run (data.main_loop);
 
-} while (!terminate);
+  fprintf(stderr, "main_loop_run returned, stopping playback\n");
 
   /* Free resources */
-  gst_object_unref (bus);
   gst_element_set_state (data.pipeline, GST_STATE_NULL);
   gst_object_unref (data.pipeline);
   return 0;
